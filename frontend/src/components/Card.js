2092d6d041a5bd8022d3c7b3004e1733d2e1a659
Will Douglas Hagedorn <whagedorn@wisc.edu>
2024-07-03 11:34:24 -0500

    Minor fixes to run properly.

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Chart from "./Chart";
 
 function Card() {
     return (
         <div className='card-layout'>
-            <Chart symbol={'IBM'}/>
+            
         </div>
 
     )
